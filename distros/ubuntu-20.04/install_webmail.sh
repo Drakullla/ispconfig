ef054347f4f4f33ee400952c749cc3f430644b21
Drakullla <66387141+Drakullla@users.noreply.github.com>
2020-06-03 23:39:42 +0300

    Update install_webmail.sh
    add tlsv1.3
    delete ssl on;
    delete tlsv1 tlsv 1.1

--- distros/ubuntu-20.04/install_webmail.sh
+++ distros/ubuntu-20.04/install_webmail.sh
@@ -23,11 +23,16 @@ InstallWebmail() {
 server {
    # SSL configuration
    listen 443 ssl;
-
-   ssl on;
-   ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+   ssl_ciphers "ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES256-GCM-SHA512:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256";
+   ssl_protocols TLSv1.2 TLSv1.3;
+      
    ssl_certificate /usr/local/ispconfig/interface/ssl/ispserver.crt;
    ssl_certificate_key /usr/local/ispconfig/interface/ssl/ispserver.key;
+
+   ssl_session_cache   shared:SSL:10m;
+   ssl_session_tickets off;
+   ssl_session_timeout 1d;
+
 
    location /roundcube {
       root /var/lib/;
